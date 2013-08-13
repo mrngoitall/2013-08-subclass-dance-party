c71f789164f5c6b5ad39b67ebcf2662fcd66de61
William Ngo <will@mrngoitall.net>
2013-08-13 15:35:22 -0700

    Added notes from lecture

--- src/dancer.js
+++ src/dancer.js
@@ -1,12 +1,13 @@
 // Creates and returns a new dancer object that can step
 var Dancer = function(top, left, timeBetweenSteps){
+  // 'this' is Object.create(Dancer.prototype); 
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
   this.setPosition(top, left);
   this._timeBetweenSteps = timeBetweenSteps;
-  this.step();
+  this.step(); //Should refer to the blinkyDancer 
   this._top = top;
   this._left = left;
 };
@@ -16,7 +17,9 @@ Dancer.prototype.step = function(){
   // it just schedules the next step
   var that = this;
   setTimeout(function() {
-    that.step();
+    that.step(); // Without using var that = this, 'this' runs in the new Dancer instance 
+    // the first time it is run, but it changes to global scope once setTimeout runs 
+    // it (in the wrong context). This means it'll just stops running altogether.
   }, this._timeBetweenSteps);
 };
 
